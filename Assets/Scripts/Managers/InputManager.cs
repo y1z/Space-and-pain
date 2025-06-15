c91b20364f65c493326854d5a73949486417d884
y1z <whyis2cool@gmail.com>
2025-06-15 11:53:10 -0600

    Start the inputManager

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+namespace Managers
+{
+    using UnityEngine.InputSystem;
+    /// <summary>
+    /// </summary>
+    public sealed class InputManager : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
