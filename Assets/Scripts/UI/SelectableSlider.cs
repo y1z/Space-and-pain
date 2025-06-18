544ebced944173194e15d237547233f94e35abfc
y1z <whyis2cool@gmail.com>
2025-06-17 19:06:19 -0600

    Start the slider class

---
+++ Assets/Scripts/UI/SelectableSlider.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace UI
+{
+    public sealed class SelectableSlider : SelectableBase
+    {
+        public override UiResult executeAction(UiAction action)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
