2309047c3bb1e57a6dfcd47c928773acf853d55d
y1z <whyis2cool@gmail.com>
2025-06-19 12:07:26 -0600

    Scripts that let me manipulate the game state

---
+++ Assets/Scripts/Util/ChangeGameState.cs
@@ -0,0 +1,44 @@
+// Ignore Spelling: Util
+
+using Managers;
+using UnityEngine;
+
+namespace Util
+{
+    public sealed class ChangeGameState : MonoBehaviour
+    {
+        private GameStates currentGameState;
+        [Tooltip("Change this to change the game state ")]
+        public GameStates desiredGameState;
+
+        private void Update()
+        {
+            if (currentGameState == desiredGameState) { return; }
+
+            SingletonManager.inst.gameManager.setState(desiredGameState);
+        }
+
+
+        #region GameManagerBoilerPlate
+
+        private void OnEnable()
+        {
+            SingletonManager.inst.gameManager.subscribe(setState);
+            setState(SingletonManager.inst.gameManager.gameState);
+        }
+
+        private void OnDisable()
+        {
+            SingletonManager.inst.gameManager.unSubscribe(setState);
+        }
+
+        private void setState(GameStates newState)
+        {
+            currentGameState = newState;
+        }
+
+        #endregion
+    }
+
+}
+
