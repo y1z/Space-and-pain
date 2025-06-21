7adebd257cb1ee02c46a0fec6f9f58e58dbff4d1
y1z <whyis2cool@gmail.com>
2025-06-21 12:11:20 -0600

    Add new tag "Enemy Projectile"

--- Assets/Scripts/Entities/EnemyShoot.cs
+++ Assets/Scripts/Entities/EnemyShoot.cs
@@ -1,3 +1,4 @@
+using Managers;
 using UnityEngine;
 
 namespace Entities
@@ -15,6 +16,22 @@ namespace Entities
                 gameObject.GetComponent<Enemy>();
                 EDebug.Assert(referenceToEnemy != null, $"this script needs a {typeof(Enemy)}", this);
             }
+        }
+
+        public void shoot()
+        {
+            Projectile[] projectile = SingletonManager.inst.enemyManager.projectiles;
+
+            for (int i = 0; i < projectile.Length; i++)
+            {
+                if (!projectile[i].gameObject.activeInHierarchy)
+                {
+                    projectile[i].gameObject.SetActive(true);
+                    projectile[i].setUp();
+                    projectile[i].teleport(spawnPoint);
+                }
+            }
+
         }
     }
 }
