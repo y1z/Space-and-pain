4c49e0c4970db62f6f34a2200bdb7cfa4659e7b6
y1z <whyis2cool@gmail.com>
2025-06-22 22:37:19 -0600

    Add script to reload scene for testing

---
+++ Assets/Scripts/ResetOnGameWonOrGameOver.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using Managers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class ResetOnGameWonOrGameOver : MonoBehaviour
+{
+    public GameStates gameState { get; private set; }
+    public float delay = 1.0f;
+
+    private void FixedUpdate()
+    {
+        if (gameState != GameStates.GAME_OVER || gameState != GameStates.WON) { return; }
+
+    }
+
+    #region GameManagerBoilerPlate
+
+    private void OnEnable()
+    {
+        SingletonManager.inst.gameManager.subscribe(setState);
+        setState(SingletonManager.inst.gameManager.gameState);
+    }
+
+    private void OnDisable()
+    {
+        SingletonManager.inst.gameManager.unSubscribe(setState);
+    }
+
+    private void setState(GameStates newState)
+    {
+        gameState = newState;
+
+        switch (gameState)
+        {
+            case GameStates.GAME_OVER:
+                StartCoroutine(reload());
+                break;
+
+            case GameStates.WON:
+                StartCoroutine(reload());
+                break;
+        }
+    }
+
+    #endregion
+
+
+    #region Coroutine
+
+    private IEnumerator reload()
+    {
+        yield return new WaitForSeconds(delay);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex);
+    }
+
+    #endregion
+
+}
