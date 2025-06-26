499c6424d3b253a75313940052ec2bb96b6f132b
y1z <whyis2cool@gmail.com>
2025-06-25 19:42:55 -0600

    Test Sound Manager, Update Sound Manager prefab

---
+++ Assets/Scripts/Test/TestSoundManager.cs
@@ -0,0 +1,25 @@
+using Managers;
+using UnityEngine;
+
+namespace Test
+{
+    using Scriptable_Objects;
+
+    public class TestSoundManager : MonoBehaviour
+    {
+        public void testBeep()
+        {
+            SingletonManager.inst.soundManager.playAudio(GameAudioType.SFX, "beep");
+        }
+
+        public void testBoom()
+        {
+            SingletonManager.inst.soundManager.playAudio(GameAudioType.SFX, "boom");
+        }
+
+        public void testGoodBeep()
+        {
+            SingletonManager.inst.soundManager.playAudio(GameAudioType.SFX, "good beep");
+        }
+    }
+}
