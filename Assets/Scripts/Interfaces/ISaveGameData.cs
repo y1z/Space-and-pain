b2eaec71288be8080d3b58f218e847a95ca67178
y1z <whyis2cool@gmail.com>
2025-07-08 17:35:48 -0600

    Changed interfaces

--- Assets/Scripts/Interfaces/ISaveGameData.cs
+++ Assets/Scripts/Interfaces/ISaveGameData.cs
@@ -1,10 +1,16 @@
-using System;
 
 namespace interfaces
 {
     public interface ISaveGameData
     {
         public string getSaveData();
+
+        public string getMetaData();
+
+        public string[] getMetaDataAndSaveData()
+        {
+            return new string[2] { getMetaData(), getSaveData() };
+        }
     }
 
 }
